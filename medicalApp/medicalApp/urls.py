102dab911322c6da8f9efe35b68bbd309b4cbbad
Ioana-Nadia <puiuioana977@yahoo.ro>
2020-11-04 12:02:40 +0200

    'switchToPostgreSQL'

--- medicalApp/medicalApp/urls.py
+++ medicalApp/medicalApp/urls.py
@@ -1,18 +1,3 @@
-"""medicalApp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,6 +8,7 @@ from medRelations.views import (
     authView,
     newPatient,
     newIntervention,
+    viewPatients,
 )
 urlpatterns = [
     path('', registrationView, name='registration'),
@@ -32,4 +18,5 @@ urlpatterns = [
     path('login', authView, name='userLogin'),
     path('newPatient', newPatient, name='newPatient'),
     path('newIntervention', newIntervention, name='newIntervention'),
+    path('viewPatients', viewPatients, name='viewPatients'),
 ]
