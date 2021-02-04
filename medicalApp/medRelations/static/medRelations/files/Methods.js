5c1e3a23934ce6e5d279aa830944b9a0287bd7cf
Ioana-Nadia <puiuioana977@yahoo.ro>
2021-02-04 13:11:23 +0200

    'addDoctorFieldAttributes'

---
+++ medicalApp/medRelations/static/medRelations/files/Methods.js
@@ -0,0 +1,34 @@
+function patientsList() {
+  document.getElementById("patientsPanel").style.display = "block";
+  document.getElementById("patientsList").style.display = "none";
+}
+
+function addNewPatient() {
+   document.getElementById("addPatient").style.display = "block";
+}
+
+function interventionsList() {
+  document.getElementById("interventionPanel").style.display = "block";
+  document.getElementById("interventionsList").style.display = "none";
+}
+
+function addNewIntervention() {
+   document.getElementById("addIntervention").style.display = "block";
+}
+
+function doctorsList() {
+  document.getElementById("doctorsPanel").style.display = "block";
+  document.getElementById("doctorsList").style.display = "none";
+}
+
+function addNewDoctor() {
+   document.getElementById("addDoctor").style.display = "block";
+}
+
+function displayPatientsList() {
+  document.getElementById("patientsList").style.display = "block";
+}
+
+function displayInterventionsList() {
+  document.getElementById("interventionsList").style.display = "block";
+}
